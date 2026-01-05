21ed99b2bd47ffe65bb27849897de327e31dec92
baran arda kandemir <baranardakandemir16@gmail.com>
2026-01-05 20:16:41 +0300

    Sinoto

---
+++ Yeni klasör (2)/Forms/FrmFilmListe.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace sinema.Forms
+{
+    partial class FrmFilmListe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmFilmListe));
+            DevExpress.XtraEditors.TableLayout.TableColumnDefinition tableColumnDefinition1 = new DevExpress.XtraEditors.TableLayout.TableColumnDefinition();
+            DevExpress.XtraEditors.TableLayout.TableColumnDefinition tableColumnDefinition2 = new DevExpress.XtraEditors.TableLayout.TableColumnDefinition();
+            DevExpress.XtraEditors.TableLayout.TableColumnDefinition tableColumnDefinition3 = new DevExpress.XtraEditors.TableLayout.TableColumnDefinition();
+            DevExpress.XtraEditors.TableLayout.TableRowDefinition tableRowDefinition1 = new DevExpress.XtraEditors.TableLayout.TableRowDefinition();
+            DevExpress.XtraEditors.TableLayout.TableRowDefinition tableRowDefinition2 = new DevExpress.XtraEditors.TableLayout.TableRowDefinition();
+            this.tileViewColumn1 = new DevExpress.XtraGrid.Columns.TileViewColumn();
+            this.tileViewColumn2 = new DevExpress.XtraGrid.Columns.TileViewColumn();
+            this.tileViewColumn3 = new DevExpress.XtraGrid.Columns.TileViewColumn();
+            this.tileViewColumn4 = new DevExpress.XtraGrid.Columns.TileViewColumn();
+            this.repositoryItemPictureEdit2 = new DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit();
+            this.repositoryItemPictureEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.filmSilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tileView1 = new DevExpress.XtraGrid.Views.Tile.TileView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnChatbotAc = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemPictureEdit2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemPictureEdit1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tileView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tileViewColumn1
+            // 
+            this.tileViewColumn1.Caption = "tileViewColumn1";
+            this.tileViewColumn1.FieldName = "FilmAdi";
+            this.tileViewColumn1.Name = "tileViewColumn1";
+            this.tileViewColumn1.Visible = true;
+            this.tileViewColumn1.VisibleIndex = 0;
+            // 
+            // tileViewColumn2
+            // 
+            this.tileViewColumn2.Caption = "tileViewColumn2";
+            this.tileViewColumn2.FieldName = "Tur";
+            this.tileViewColumn2.Name = "tileViewColumn2";
+            this.tileViewColumn2.Visible = true;
+            this.tileViewColumn2.VisibleIndex = 1;
+            // 
+            // tileViewColumn3
+            // 
+            this.tileViewColumn3.Caption = "tileViewColumn3";
+            this.tileViewColumn3.FieldName = "SureDakika";
+            this.tileViewColumn3.Name = "tileViewColumn3";
+            this.tileViewColumn3.Visible = true;
+            this.tileViewColumn3.VisibleIndex = 2;
+            // 
+            // tileViewColumn4
+            // 
+            this.tileViewColumn4.Caption = "tileViewColumn4";
+            this.tileViewColumn4.ColumnEdit = this.repositoryItemPictureEdit2;
+            this.tileViewColumn4.FieldName = "AfisYolu";
+            this.tileViewColumn4.MinWidth = 27;
+            this.tileViewColumn4.Name = "tileViewColumn4";
+            this.tileViewColumn4.Visible = true;
+            this.tileViewColumn4.VisibleIndex = 3;
+            this.tileViewColumn4.Width = 100;
+            // 
+            // repositoryItemPictureEdit2
+            // 
+            this.repositoryItemPictureEdit2.Name = "repositoryItemPictureEdit2";
+            // 
+            // repositoryItemPictureEdit1
+            // 
+            this.repositoryItemPictureEdit1.Name = "repositoryItemPictureEdit1";
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("gridControl1.BackgroundImage")));
+            this.gridControl1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.gridControl1.ContextMenuStrip = this.contextMenuStrip1;
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.EmbeddedNavigator.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.gridControl1.Location = new System.Drawing.Point(0, 0);
+            this.gridControl1.MainView = this.tileView1;
+            this.gridControl1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
+            this.repositoryItemPictureEdit1,
+            this.repositoryItemPictureEdit2});
+            this.gridControl1.Size = new System.Drawing.Size(901, 544);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.tileView1});
+            this.gridControl1.MouseEnter += new System.EventHandler(this.gridControl1_MouseEnter);
+            this.gridControl1.MouseLeave += new System.EventHandler(this.gridControl1_MouseLeave);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.filmSilToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(113, 26);
+            // 
+            // filmSilToolStripMenuItem
+            // 
+            this.filmSilToolStripMenuItem.Name = "filmSilToolStripMenuItem";
+            this.filmSilToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.filmSilToolStripMenuItem.Text = "Film Sil";
+            this.filmSilToolStripMenuItem.Click += new System.EventHandler(this.silToolStripMenuItem_Click);
+            // 
+            // tileView1
+            // 
+            this.tileView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.tileViewColumn1,
+            this.tileViewColumn2,
+            this.tileViewColumn3,
+            this.tileViewColumn4});
+            this.tileView1.ColumnSet.BackgroundImageColumn = this.tileViewColumn4;
+            this.tileView1.DetailHeight = 538;
+            this.tileView1.GridControl = this.gridControl1;
+            this.tileView1.Name = "tileView1";
+            this.tileView1.OptionsBehavior.AllowSmoothScrolling = true;
+            this.tileView1.OptionsTiles.ItemBackgroundImageScaleMode = DevExpress.XtraEditors.TileItemImageScaleMode.Stretch;
+            this.tileView1.OptionsTiles.ItemSize = new System.Drawing.Size(400, 400);
+            this.tileView1.OptionsTiles.RowCount = 0;
+            tableColumnDefinition1.Length.Value = 0D;
+            tableColumnDefinition2.Length.Value = 224D;
+            tableColumnDefinition3.Length.Value = 0D;
+            this.tileView1.TileColumns.Add(tableColumnDefinition1);
+            this.tileView1.TileColumns.Add(tableColumnDefinition2);
+            this.tileView1.TileColumns.Add(tableColumnDefinition3);
+            tableRowDefinition1.Length.Value = 104D;
+            tableRowDefinition2.Length.Value = 0D;
+            this.tileView1.TileRows.Add(tableRowDefinition1);
+            this.tileView1.TileRows.Add(tableRowDefinition2);
+            this.tileView1.ItemClick += new DevExpress.XtraGrid.Views.Tile.TileViewItemClickEventHandler(this.tileView1_ItemClick);
+            this.tileView1.CustomUnboundColumnData += new DevExpress.XtraGrid.Views.Base.CustomColumnDataEventHandler(this.TileView1_CustomUnboundColumnData);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnChatbotAc
+            // 
+            this.btnChatbotAc.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.btnChatbotAc.Appearance.Font = new System.Drawing.Font("Times New Roman", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnChatbotAc.Appearance.ForeColor = System.Drawing.Color.Black;
+            this.btnChatbotAc.Appearance.Options.UseBackColor = true;
+            this.btnChatbotAc.Appearance.Options.UseFont = true;
+            this.btnChatbotAc.Appearance.Options.UseForeColor = true;
+            this.btnChatbotAc.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnChatbotAc.BackgroundImage")));
+            this.btnChatbotAc.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnChatbotAc.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.Simple;
+            this.btnChatbotAc.Location = new System.Drawing.Point(708, 429);
+            this.btnChatbotAc.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnChatbotAc.Name = "btnChatbotAc";
+            this.btnChatbotAc.Size = new System.Drawing.Size(156, 79);
+            this.btnChatbotAc.TabIndex = 1;
+            this.btnChatbotAc.Text = "Sinoto";
+            this.btnChatbotAc.Click += new System.EventHandler(this.btnChatbotAc_Click);
+            // 
+            // FrmFilmListe
+            // 
+            this.Appearance.Options.UseFont = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayoutStore = System.Windows.Forms.ImageLayout.Tile;
+            this.BackgroundImageStore = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImageStore")));
+            this.ClientSize = new System.Drawing.Size(901, 544);
+            this.Controls.Add(this.btnChatbotAc);
+            this.Controls.Add(this.gridControl1);
+            this.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FrmFilmListe";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmFilmListe";
+            this.Activated += new System.EventHandler(this.FrmFilmListe_Activated);
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemPictureEdit2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemPictureEdit1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.tileView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Tile.TileView tileView1;
+        private DevExpress.XtraGrid.Columns.TileViewColumn tileViewColumn1;
+        private DevExpress.XtraGrid.Columns.TileViewColumn tileViewColumn2;
+        private DevExpress.XtraGrid.Columns.TileViewColumn tileViewColumn3;
+        private DevExpress.XtraGrid.Columns.TileViewColumn tileViewColumn4;
+        private DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit repositoryItemPictureEdit1;
+        private DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit repositoryItemPictureEdit2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem filmSilToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private DevExpress.XtraEditors.SimpleButton btnChatbotAc;
+    }
+}
---
+++ Yeni klasör (2)/Forms/FrmFilmListe.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraGrid.Columns;
+using DevExpress.Data;
+using sinema.Models;
+using DevExpress.XtraEditors;
+namespace sinema.Forms
+{
+    public partial class FrmFilmListe : DevExpress.XtraEditors.XtraForm
+    {
+        // Veritabanı bağlantısı tüm metotların dışında olmalı
+        SinemaOtomasyonDbEntities db = new SinemaOtomasyonDbEntities();
+
+        public FrmFilmListe()
+        {
+            InitializeComponent();
+            Listele();
+            ResimSutunuHazirla();
+        }
+
+        void Listele()
+        {
+            // DB context'i yenileyerek veritabanından güncel veriyi çekelim
+            db = new SinemaOtomasyonDbEntities();
+            gridControl1.DataSource = db.TblFilmler.ToList();
+
+            // GridView'i verileri yeniden çizmesi için tetikleyelim
+            gridControl1.RefreshDataSource();
+        }
+        void ResimSutunuHazirla()
+        {
+            if (tileView1.Columns["SanalResim"] == null)
+            {
+                TileViewColumn colResim = new TileViewColumn();
+                colResim.FieldName = "SanalResim";
+                colResim.UnboundType = UnboundColumnType.Object;
+                colResim.Visible = false;
+                tileView1.Columns.Add(colResim);
+                tileView1.ColumnSet.BackgroundImageColumn = colResim;
+            }
+            tileView1.CustomUnboundColumnData += TileView1_CustomUnboundColumnData;
+        }
+
+        private void TileView1_CustomUnboundColumnData(object sender, DevExpress.XtraGrid.Views.Base.CustomColumnDataEventArgs e)
+        {
+            if (e.Column.FieldName == "SanalResim" && e.IsGetData)
+            {
+                string dosyaYolu = Convert.ToString(tileView1.GetListSourceRowCellValue(e.ListSourceRowIndex, "AfisYolu"));
+                if (!string.IsNullOrEmpty(dosyaYolu) && System.IO.File.Exists(dosyaYolu))
+                {
+                    try { e.Value = Image.FromFile(dosyaYolu); } catch { e.Value = null; }
+                }
+            }
+        }
+        private void tileView1_ItemClick(object sender, DevExpress.XtraGrid.Views.Tile.TileViewItemClickEventArgs e)
+        {
+            // Hata: image_edec29.png'deki RowHandle tanımı hatası için düzeltme:
+            var secilenFilm = tileView1.GetRow(e.Item.RowHandle) as TblFilmler;
+
+            if (secilenFilm != null)
+            {
+                FrmFilmDetay frm = new FrmFilmDetay();
+                frm.gelenFilmId = secilenFilm.ID;
+                frm.Show();
+            }
+        }
+        // SİLME METODU
+        private void silToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var secilenFilm = tileView1.GetFocusedRow() as TblFilmler;
+            if (secilenFilm == null) return;
+
+            if (MessageBox.Show(secilenFilm.FilmAdi + " ve bağlı tüm kayıtlar silinsin mi?", "Onay", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                try
+                {
+                    int fId = secilenFilm.ID;
+
+                    // 1. Önce bu filme ait biletleri (seanslar üzerinden) temizle
+                    var seanslar = db.TblSeanslar.Where(x => x.FilmID == fId).ToList();
+                    foreach (var s in seanslar)
+                    {
+                        var biletler = db.TblBiletler.Where(x => x.SeansID == s.ID).ToList();
+                        foreach (var b in biletler) db.TblBiletler.Remove(b);
+                        db.TblSeanslar.Remove(s); // Sonra seansı temizle
+                    }
+
+                    // 2. En son filmi temizle
+                    var film = db.TblFilmler.Find(fId);
+                    db.TblFilmler.Remove(film);
+
+                    db.SaveChanges();
+                    Listele();
+                    MessageBox.Show("Film ve ilişkili tüm veriler başarıyla temizlendi.");
+                }
+                catch (Exception ex) { MessageBox.Show("Hata oluştu: " + ex.Message); }
+            }
+        }
+
+        private void FrmFilmListe_Activated(object sender, EventArgs e)
+        {
+            Listele();
+        }
+     
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+            {
+                // tileView1 sizin Grid üzerindeki TileView'ınızın adıdır
+                int currentRow = tileView1.FocusedRowHandle;
+                int totalRows = tileView1.RowCount;
+
+
+                // Eğer son filme gelindiyse başa dön, gelmediyse bir sonrakine geç
+                if (currentRow < totalRows - 1)
+                {
+                    tileView1.FocusedRowHandle = currentRow + 1;
+                }
+                else
+                {
+                    tileView1.FocusedRowHandle = 0; // Başa dön
+                }
+            }
+
+        private void gridControl1_MouseEnter(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void gridControl1_MouseLeave(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        
+            private void btnChatbotAc_Click(object sender, EventArgs e)
+            {
+                // Formun zaten açık olup olmadığını kontrol etmek iyi bir mühendislik yaklaşımıdır
+                FrmChatbot frm = new FrmChatbot();
+                frm.Show(); // ShowDialog() derseniz arkadaki formlara tıklayamazsınız, Show() daha iyidir.
+            }
+        
+    }
+}
